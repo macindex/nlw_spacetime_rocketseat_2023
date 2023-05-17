0e51c4f9a2980eb678969e05d1f15b96d27d3837
macindex <machollanda@gmail.com>
2023-05-16 23:03:05 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+// const nome: string = 'Claudio Marcio'
+
+// console.log(nome)
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+    const users = await prisma.user.findMany()
+
+    return users
+})
+
+// app.get('/hello', () => {
+//     return 'Helo world!'
+// })
+
+app.listen({
+    port: 3333,
+}).then(()=>{
+    console.log('HTTP server running on http://localhost:3333')
+})

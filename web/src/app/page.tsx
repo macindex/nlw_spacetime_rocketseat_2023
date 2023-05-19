b745269007a8de8b98cc0c66af54188e1a2a20b1
macindex <machollanda@gmail.com>
2023-05-18 23:34:32 -0300

    backgroundImage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,7 +3,9 @@ export default function Home() {
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        {/* Blur */}
         <div className="blur-full absolute right-0 top-1/2 h-[288px] w-[526] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50" />
+        <div className="bg-stripes absolute bottom-0 right-0 top-0 w-2"></div>
       </div>
       {/* Right */}
       <div className="flex flex-col p-16">

8c8a24bc74869903ecbb813b369519358e01bcd1
macindex <machollanda@gmail.com>
2023-05-18 22:50:39 -0300

    blur background

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Your time capsule</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="blur-full absolute right-0 top-1/2 h-[288px] w-[526] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50" />
+      </div>
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            You still not register anyone remember, start{''}{' '}
+            <a href="" className="underline hover:text-gray-50">
+              create now
+            </a>{' '}
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }

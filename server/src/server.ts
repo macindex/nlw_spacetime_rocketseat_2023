7f85aab8fe632481110a194f37e4d11092e46cbc
macindex <machollanda@gmail.com>
2023-05-27 21:55:40 -0300

    accessToken

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -10,6 +12,7 @@ app.register(cors, {
 })
 
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app.listen({
     port: 3333,
